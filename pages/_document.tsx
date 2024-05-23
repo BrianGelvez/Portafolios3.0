8cb5297750418ccd9056fbf2df40a1a6748150c2
Brian Gelvez <briannn97@gmail.com>
2024-05-22 23:16:40 -0300

    fix: Cambio de imagen de metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -63,8 +63,8 @@ export default function Document() {
                 <meta property="og:title" content="Brian Gelvez" key="title"/>
                 {/* <meta property="og:description"
                       content="Brian Gelvez - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey."/> */}
-                <meta property="og:image" content="https://avatars.githubusercontent.com/u/75434191?v=4"/>
-                <meta property="og:image:secure_url" content="https://avatars.githubusercontent.com/u/75434191?v=4"/>
+                <meta property="og:image" content="https://github.com/BrianGelvez/Portafolios3.0/blob/main/public/brianmilitar.jpg?raw=true"/>
+                <meta property="og:image:secure_url" content="https://github.com/BrianGelvez/Portafolios3.0/blob/main/public/brianmilitar.jpg?raw=true"/>
                 <meta property="og:image:type" content="image/jpeg"/>
                 <meta property="og:image:alt" content="Brian Gelvez"/>
                 <meta property="og:image:width" content="300"/>
